ecd571e43f83de88c41810999f61bddf09127b7c
Anurag Chatap <achatap@gmail.com>
2020-07-25 11:38:20 +0530

    Update ScreenShot.java

--- src/test/java/accelerate/ScreenShot.java
+++ src/test/java/accelerate/ScreenShot.java
@@ -1,24 +1,16 @@
 package accelerate;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.opera.OperaDriver;
-
 import com.assertthat.selenium_shutterbug.core.Shutterbug;
 import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
 
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 public class ScreenShot {
 
@@ -26,7 +18,7 @@ public class ScreenShot {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = null ;
-		String browser= "chrome";
+		String browser= "opera";
 		
 		switch(browser) {
 		case "chrome":
@@ -42,7 +34,10 @@ public class ScreenShot {
 	    	
 		case "ie":
 			System.setProperty("webdriver.ie.driver","C:\\Webdrivers\\IEDriverServer.exe");
-	    	driver = new InternetExplorerDriver();
+			InternetExplorerOptions capabilities = new InternetExplorerOptions();
+			capabilities.ignoreZoomSettings();
+			driver = new InternetExplorerDriver(capabilities);
+			
 	    	break;
 	    	
 		case "edge":
@@ -54,15 +49,17 @@ public class ScreenShot {
 			System.setProperty("webdriver.opera.driver","C:\\Webdrivers\\operadriver.exe");
 	    	driver = new OperaDriver();
 	    	break;
+	    
+	    	
 		}
 		
     	
-        driver.get("http://www.filippobello.com/portfolio");
+        driver.get("https://programesecure.com");
         driver.manage().window().maximize();     
      	Shutterbug.shootPage(driver, ScrollStrategy.WHOLE_PAGE, 1000, true).save("E:\\NewElementScreenshot.jpeg");  
         driver.close();
         
-        
+      
 
 	}
 
